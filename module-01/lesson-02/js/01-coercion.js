4dfcc8c14cbb932c0af88190d16c13d2bbc2a17c
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2024-12-24 15:06:56 +0200

    prepared module 01 lesson 02

---
+++ module-01/lesson-02/js/01-coercion.js
@@ -0,0 +1,26 @@
+/**
+ * Перетворення типів: числа
+ *
+ * - Явне перетворення типів
+ * - Неявне перетворення типів
+ */
+
+// console.log(Number('5')); // 5
+// console.log(Number(true)); // 1
+// console.log(Number(false)); // 0
+// console.log(Number(null)); // 0
+// console.log(Number(undefined)); // NaN
+// console.log(Number('Jacob')); // NaN
+// console.log(Number('25px')); // NaN
+
+// const result1 = isNaN('25px');
+// const result2 = Number.isNaN('25px');
+// console.log('result1:', result1);
+// console.log('result2:', result2);
+
+// console.log('5' * 2); // 10
+// console.log('10' - 5); // 5
+// console.log(5 + true); // 6
+// console.log('5' + true); // 6
+// console.log(5 - true); // 4
+// console.log('5px' - false); // NaN
---
+++ module-01/lesson-02/js/03-numbers.js
@@ -0,0 +1,27 @@
+/**
+ * Арифметичні функції
+ *
+ * - Number.parseInt
+ * - Number.parseFloat
+ * - Math
+ */
+
+// let elementWidth = '50px';
+// console.log('elementWidth: ', Number.parseInt(elementWidth));
+// console.log('elementWidth: ', Number.parseInt('50рх20рх'));
+
+// let elementHeight = '200.74px';
+// console.log('elementHeight: ', Number.parseFloat(elementHeight));
+// console.log('elementHeight: ', Number.parseFloat('200.74.87deg'));
+// console.log('elementHeight: ', Number.parseFloat('87deg'));
+
+/**
+ * Напиши скрипт, який виводить у консоль заокруглені вгору/вниз
+ * значення змінної value. Використовуй методи Math.floor(), Math.ceil()
+ * та Math.round(). Перевір що буде в консолі при значеннях 27.3 та 27.9.
+ */
+
+const value = 27.3;
+// console.log("floor", Math.floor(value));
+// console.log("ceil", Math.ceil(value));
+// console.log("round", Math.round(value));

47b632702f5eeb9b8808cdd9e15d7cb7fb07d65d
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2025-02-27 00:57:10 +0200

    finished module 09 lesson 01

--- module-09/lesson-01/js/01-localstorage.js
+++ module-09/lesson-01/js/01-localstorage.js
@@ -1,3 +1,35 @@
+// import "./userProfile.json";
+// console.log(profile);
+
+// const userName = "Mango";
+
+// const userProfile = {
+//   name: "Mango",
+//   age: 24,
+//   hobbies: ["tennis", "football", "swimming"],
+//   checkAge() {
+//     if (this.age < 18) {
+//       console.log("Child");
+//     } else {
+//       console.log("Adult");
+//     }
+//   },
+// };
+
+// function checkAge(age) {
+//   if (age < 18) {
+//     console.log("Child");
+//   } else {
+//     console.log("Adult");
+//   }
+// }
+
+// const res = JSON.stringify(userProfile);
+// console.log(JSON.stringify(checkAge));
+
+// const parsedData = JSON.parse(res);
+// console.log(" parsedData:", parsedData);
+
 /**
  * LocalStorage
  */
@@ -5,20 +37,48 @@
 const LS_KEY = "Array of names";
 const names = ["Alice", "Kate", "Emma"];
 
+const chief = {
+  cook: {
+    name: "Heston Chan",
+    imgUrl: "https://tasty-treats-backend.p.goit.global/HestonChan.png",
+    imgWebpUrl: "https://tasty-treats-backend.p.goit.global/HestonChan.webp",
+  },
+  topic: {
+    name: "Grilled, smoky, delicious barbecue",
+    area: "Spanish",
+    previewUrl: "https://tasty-treats-backend.p.goit.global/previewDish1.png",
+    imgUrl: "https://tasty-treats-backend.p.goit.global/imgDish1.png",
+    imgWebpUrl: "https://tasty-treats-backend.p.goit.global/imgDish1.webp",
+    previewWebpUrl:
+      "https://tasty-treats-backend.p.goit.global/previewDish1.webp",
+  },
+  _id: "647495d0c825f1570b04182d",
+};
+
+const lsData = localStorage.setItem("items", "Part 1, Part 2");
+console.log("lsData:", lsData);
+
 /**
  * Збереження
  * Чому треба використовувати метод JSON.stringify
  */
+const stringifiedNames = JSON.stringify(names);
+localStorage.setItem(LS_KEY, stringifiedNames);
 
 /**
  * Читання
  * Чому треба використовувати метод JSON.parse
  */
+const lsNames = localStorage.getItem(LS_KEY);
+const parsedLSData = JSON.parse(lsNames);
+console.log(" parsedLSData:", parsedLSData);
 
 /**
  * Видалення
  */
-
+setInterval(() => {
+  localStorage.removeItem("items");
+}, 3000);
 /**
  * LocalStorage не може зберігати функції
  */
@@ -34,3 +94,10 @@ const calculator = {
     return this.a + this.b;
   },
 };
+localStorage.setItem("calculator", JSON.stringify(calculator));
+const res1 = localStorage.getItem("calculator");
+console.log(" res1:", res1);
+
+setInterval(() => {
+  localStorage.setItem(LS_KEY, JSON.stringify(chief));
+}, 5000);
--- module-09/lesson-01/js/02-feedback.js
+++ module-09/lesson-01/js/02-feedback.js
@@ -1,7 +1,15 @@
+//* Form data constructor
+//* (https://developer.mozilla.org/en-US/docs/Web/API/FormData/FormData)
+
 const STORAGE_KEY = "feedback-msg";
 
 const form = document.querySelector(".feedback-form");
 const textarea = form.querySelector("textarea");
+
+populateTextarea();
+
+form.addEventListener("submit", onFormSubmit);
+textarea.addEventListener("input", onTextareaInput);
 
 /**
  * - Скасовуємо стандартну поведінку
@@ -9,12 +17,54 @@ const textarea = form.querySelector("textarea");
  * - Очищуємо форму
  */
 
+function onFormSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+
+  const { name, message } = form.elements;
+
+  // const data = new FormData(form);
+
+  // const formData = {};
+
+  // for (const [key, value] of data) {
+  //   formData[key] = value;
+  // }
+
+  // console.log(formData);
+
+  const formData = {
+    userName: name.value,
+    userMessage: message.value,
+  };
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+}
+
 /**
  * - Отримуємо значення поля
  * - Зберігаємо його у сховище
  */
+function onTextareaInput(event) {
+  const msg = event.target.value;
+  localStorage.setItem(STORAGE_KEY, msg);
+}
 
 /**
  * - Отримуємо значення зі сховища
  * - Якщо там щось було, оновлюємо DOM
  */
+function populateTextarea() {
+  const savedMsg = localStorage.getItem(STORAGE_KEY);
+
+  if (!savedMsg) {
+    return;
+  }
+
+  textarea.value = savedMsg;
+
+  // textarea.value = localStorage.getItem(STORAGE_KEY) ?? "";
+}

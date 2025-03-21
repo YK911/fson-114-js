a8a7d4e3d939b26af9e6267d0e57e93ec9ad7e46
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2025-03-21 19:30:09 +0200

    delete all codebase

--- module-12/lesson-02/src/js/helpers/truncateText.js
+++
@@ -1,8 +0,0 @@
-export function truncate(text, symbolsLimit = 90) {
-  if (!text) {
-    return text;
-  }
-  return text.length <= symbolsLimit
-    ? text
-    : `${text.slice(0, symbolsLimit - 3)}...`;
-}
--- module-12/lesson-02/src/js/refs.js
+++
@@ -1,6 +0,0 @@
-export const refs = {
-  form: document.querySelector(".js-search-form"),
-  container: document.querySelector(".js-cards-container"),
-  loadMoreBtn: document.querySelector(".js-load-more"),
-  loader: document.querySelector(".js-loader"),
-};
